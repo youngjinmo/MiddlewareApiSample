53df904969d8f056012625e8ed7f7376f03298ee
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-25 23:15:13 +0900

    initialize project

---
+++ src/main/java/com/devandy/MiddlewareApiTutorial/MiddlewareApiTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.devandy.MiddlewareApiTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareApiTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareApiTutorialApplication.class, args);
+	}
+
+}
