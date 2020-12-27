5517dc8804f46d55259e9138bd1d5284caf470c1
Youngjin Mo <dev.youngjinmo@gmail.com>
2020-12-27 14:25:21 +0900

    Fix : 오류 수정

--- src/main/java/com/devandy/MiddlewareApiTutorial/service/ApiService.java
+++ src/main/java/com/devandy/MiddlewareApiTutorial/service/ApiService.java
@@ -29,14 +29,15 @@ public class ApiService {
             int idxValue=0;
             String name = "";
             String value = "";
+
             if(tempArr[i].contains("value")){
                 // returns index of name and value
                 idxName = tempArr[i].indexOf("name");
                 idxValue = tempArr[i].indexOf("value");
 
                 // returns value of name and value
-                name = tempArr[i].substring(idxName, tempArr[i].lastIndexOf("'"));
-                value = tempArr[i].substring(idxValue, tempArr[i].lastIndexOf("'"));
+                name = tempArr[i].substring(idxName+6, idxValue-2);
+                value = tempArr[i].substring(idxValue+7, tempArr[i].length()-2);
 
                 response.put(name, value);
             }
@@ -44,38 +45,58 @@ public class ApiService {
         return response;
     }
 
+    /**
+     *  json을 VO로 타입 캐스팅
+     *
+     * @param param
+     * @return
+     */
     public static TestVO convertToVOFromMap(Map<String, String> param){
         ObjectMapper mapper = new ObjectMapper();
         return mapper.convertValue(param, TestVO.class);
     }
 
+    /**
+     *  비즈니스 로직에 의해 VO 가공
+     *
+     * @param param
+     * @return
+     */
     public static TestVO businessService(TestVO param){
         TestVO vo = new TestVO();
         if(param.getNation().equals("Korea")){
             vo.setName("모영진");
-            vo.setAge(30);
+            vo.setAge("30");
             vo.setPosition("수비수");
-            vo.setGoal(10);
+            vo.setGoal("10");
             vo.setNation("대한민국");
         } else {
-            vo.setName("Youngjin Mo");
-            vo.setAge(29);
+            vo.setName("YoungjinMo");
+            vo.setAge("29");
             vo.setPosition("Defender");
-            vo.setGoal(10);
+            vo.setGoal("10");
             vo.setNation("Korea");
         }
         return vo;
     }
 
+    /**
+     *  바디에 HTML로 던져주기 위해 다시 문자열로 형변환
+     *
+     * @param param
+     * @return
+     */
     public static String returnToBody(TestVO param){
-        return "<html>"
-                + "<body>"
-                + "  <input type='text' name='name' value'"+param.getName()+"'>"
-                + "  <input type='text' name='age' value'"+param.getAge()+"'>"
-                + "  <input type='text' name='position' value'"+param.getPosition()+"'>"
-                + "  <input type='text' name='goal' value'"+param.getGoal()+"'>"
-                + "  <input type='text' name='nation' value'"+param.getNation()+"'>"
-                + "</body>"
+        return "<html>\n"
+                + "<body>\n"
+                + "  <form name='test' method='post' action='http://127.0.0.1:8080/api'>"
+                + "    <input type='text' name='name' value='"+param.getName()+"'>\n"
+                + "    <input type='number' name='age' value="+param.getAge()+">\n"
+                + "    <input type='text' name='position' value="+param.getPosition()+"'>\n"
+                + "    <input type='number' name='goal' value="+param.getGoal()+">\n"
+                + "    <input type='text' name='nation' value='"+param.getNation()+"'>\n"
+                + "  </form>"
+                + "</body>\n"
                 + "</html>";
     }
 
--- src/main/java/com/devandy/MiddlewareApiTutorial/vo/TestVO.java
+++ src/main/java/com/devandy/MiddlewareApiTutorial/vo/TestVO.java
@@ -1,13 +1,19 @@
 package com.devandy.MiddlewareApiTutorial.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class TestVO {
+    @JsonProperty(value = "name")
     private String name;
-    private int age;
+    @JsonProperty(value = "age")
+    private String age;
+    @JsonProperty(value = "nation")
     private String nation;
+    @JsonProperty(value = "position")
     private String position;
-    private int goal;
+    @JsonProperty(value = "goal")
+    private String goal;
 }
